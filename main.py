da05f3bdb5393a4faacb407b41d4fe0113ea8418
Kevin <i19045@hb.dhbw-stuttgart.de>
2022-03-31 17:10:46 +0200

    Working MainWindow
    Working MainWindow

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ import sys
 from ui.MainWindow import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+#if __name__ == '__main__':
+#    app = QtWidgets.QApplication(sys.argv)
+
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
-        
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
+        self.setWindowTitle("Telefonnummer optimieren")
 
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
+    main()
