347ff1edbfb12c0f164c683124794237e6f2224e
Kevin <i19045@hb.dhbw-stuttgart.de>
2022-03-31 16:57:28 +0200

    Window Test
    Window Test

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import sys
+
+from ui.MainWindow import Ui_MainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        
+
+
 if __name__ == '__main__':
-    print('AY AY AY')
+    app = QtWidgets.QApplication(sys.argv)
